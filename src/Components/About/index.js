654516826165986ac112a86b6ac30fa37a3c52b2
faris114 <farisabutaya@gmail.com>
2019-10-21 17:13:16 +0300

    front end basic form routes file structure is done

---
+++ src/Components/About/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1 style={{ backgroundColor: "yellow", margin: 10, height: 700 }}>
+        about
+      </h1>
+    </div>
+  );
+}
---
+++ src/Components/Why/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Why() {
+  return (
+    <div>
+      <h1 style={{ backgroundColor: "red", margin: 10, height: 700 }}>Why</h1>
+    </div>
+  );
+}
